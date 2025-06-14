db4d0e4ec49f9cde0d06f6e8ba3175a11ae3b6c5
Faisal <24-59046-3@student.aiub.edu>
2025-06-14 20:06:22 +0600

    Delete SimpleInterestApp directory

--- SimpleInterestApp/SimpleInterestApp/Start.java
+++
@@ -1,9 +0,0 @@
-import GUI.InterestManagementFrame;
-
-import javax.swing.SwingUtilities;
-
-public class Start {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new InterestManagementFrame().setVisible(true));
-    }
-}
